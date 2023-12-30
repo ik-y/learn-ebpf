c61137c5ca1f7ddef6bf39986cd559d59233231b
ik-y <yuu@imago.co.jp>
2023-12-30 13:44:21 +0000

    Add comment to hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,6 @@
 from bcc import BPF
 
+# eBPF program to run with kernel
 program = r"""
 int hello(void *ctx){
     bpf_trace_printk("Hello, world!");
@@ -7,9 +8,12 @@ int hello(void *ctx){
 }
 """
 
+# Attach eBPF C program to "execve" syscall
 b = BPF(text=program)
 syscall_execve = b.get_syscall_fnname("execve")
 b.attach_kprobe(event=syscall_execve, fn_name="hello")
 
+# Print tracing result
+# execve runs program, so new program run will cause log "Hello, world!"
 b.trace_print()
 
